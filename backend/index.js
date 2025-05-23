be6124b9df2837800150dee3d9ab72d48711a278
thomasdns <dasneves.thomas.sio@gmail.com>
2025-05-23 12:02:50 +0200

    config-mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
 require("dotenv").config();
- 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
- 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
- 
+
+// Connexion à MongoDB
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
- 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
